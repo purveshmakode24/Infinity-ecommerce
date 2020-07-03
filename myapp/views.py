40e78405da003de49174da1fa871c2dd1f18f1e5
Purvesh Makode <makodepurvesh@gmail.com>
2020-07-03 23:28:16 +0530

    Cleaning code

--- myapp/views.py
+++ myapp/views.py
@@ -89,26 +89,6 @@ def products(request):
 
 
 def products_cat(request, cat_id, c_slug):
-    # category_men = Category.objects.filter(id=1)
-    # category_women = Category.objects.filter(id=2)
-    # category_acc = Category.objects.filter(id=3)
-    # all_products = Product.objects.all()
-
-    # women_products = Product.objects.filter(category=2)
-    # all_accessories = Product.objects.filter(category=3)
-
-    # acc_bags = Product.objects.filter(subcategory='bag')
-    # acc_shoes = Product.objects.filter(subcategory='shoes')
-    # acc_watches = Product.objects.filter(subcategory='watches')
-
-    # cat_id =1 for men, cat_id=2 women, cat_id=3 for Accessories
-    # category = Category.objects.all()
-    # for x in category:
-    #     modal_cat_id = x.id
-    #     modal_cat_slug = x.slug
-    #
-    # if modal_cat_id == cat_id and modal_cat_slug == slug:
-    #     print(modal_cat_id)
     current_cat_products = Product.objects.filter(category_id=cat_id)
     try:
         user = User.objects.get(username=request.user)
@@ -122,27 +102,6 @@ def products_cat(request, cat_id, c_slug):
         return render(request, 'product-category.html', context)
     except User.DoesNotExist:
         user = None
-        # else:
-        #     return HttpResponse("Error:404")
-        # if cat_id == 1:
-        #     context = {
-        #         'current_cat_products': current_cat_products,
-        #         'slug': slug,
-        #         'cat_id': cat_id,
-        #     }
-        #
-        # elif cat_id == 2:
-        #     context = {
-        #         'current_cat_products': current_cat_products,
-        #         'slug': slug,
-        #         'cat_id': cat_id,
-        #     }
-        # else:
-        #     context = {
-        #         'current_cat_products': current_cat_products,
-        #         'slug': slug,
-        #         'cat_id': cat_id,
-        #     }
         context = {
             'current_cat_products': current_cat_products,
             'slug': c_slug,
@@ -179,7 +138,7 @@ def product_overview(request, cat_id, c_slug, p_id, p_slug):
         }
         return render(request, 'overview.html', context)
 
-
+    
 def about(request):
     return render(request, 'about.html', {})
 
@@ -201,43 +160,18 @@ def cart(request, username):
     current_user_cart_entries = user.current_user_cart.all()
     current_user_cart_entries_count = user.current_user_cart.all().count()
     print(current_user_cart_entries)
-    print(current_user_cart_entries_count)
-    # for item in current_user_cart_entries:
-    #     item_count = item.count
-    #     print("count in cart:::", item_count)
-    #     entry_price = item.entry_price
+
     context = {
         'current_user_cart_entries': current_user_cart_entries,
         'current_user_cart_entries_count': current_user_cart_entries_count,
-        # 'item_count': item_count,
-        # 'entry_price': entry_price,
     }
     return render(request, 'shoping-cart.html', context)
 
 
 @login_required
 def add_to_cart(request, user_id, cat_id, p_id):
-    # if request.method == 'POST':
-    #     req_product_size = request.POST.get('size')
-    #     req_product_color = request.POST.get('color')
-    #     quantity_of_items = request.POST.get('num-product')
-    #     try:
-    #         form = Cart(user_id=user_id, product_id=p_id, size=req_product_size, color=req_product_color,
-    #                     count=quantity_of_items)
-    #         form.save()
-    #         messages.success(request, f'Post has been Successfully Added!')
-    #     except Exception as e:
-    #         print(e)
-    # else:
-    #     # return render(request, '404.html')
-    #     # return redirect('error_404')
-    #     return HttpResponse('Error:404')
-    # return redirect('infinity_home')
-
-    # if cat_id == 3: remove size (size there is no size in accessoriess categories)
 
     if request.method == 'POST':
-        # products = Product.objects.all()
         form = AddToCartForm(request.POST)
         if form.is_valid():
             count = form.cleaned_data['count']
